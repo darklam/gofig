3d21a518173936fc2fc9d1e34e1c689d14c2d9fd
John Lamis <lamisjohn@gmail.com>
2023-08-28 22:58:56 +0300

    refactor the run tests script to take cmd parameters

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-go generate ./...
-go test $(go list ./... | grep -v tools)
+for arg in "$@"; do
+  case $arg in
+    -cover) coverage=true;;
+    -gen) generate=true;;
+  esac
+done
+
+if [[ "$generate" = true ]]; then
+  ./generate_mocks.sh
+fi
+
+if [[ "$coverage" = true ]]; then
+  go test ./... -covermode=count -coverprofile=coverage.out
+else
+  go test ./...
+fi
